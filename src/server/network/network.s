3c110623aafdf88bb4883803dfb31c05e57203dc
BitBlitObviMormon <jasonhhotstuff@aol.com>
2017-06-28 15:31:01 -0700

    Changed player layout

--- src/server/network/network.s
+++ src/server/network/network.s
@@ -3,23 +3,36 @@
 .include "lib/macrolib/macrolib.inc"
 
 /* CONSTANTS */
-.set	PORT,      7777
-.set	BACKLOG,   2048 // How much data each socket can read
-.set	BLOCK,        0
-.set	SOCKSPACE, 1024 // How much space to allocate for sockets
+.set	PORT,           7777 // The server's port
+.set	BACKLOG,        2048 // How much data each socket can read
+.set	BLOCKS,            0 // Whether or not to allow blocking server
+.set	BLOCKC,            1 // Whether or not to allow blocking clients
+.set	MAXPLAYERS,       16 // The max amount of players that can join
+.set	MAXNAMECHAR,      15 // The max amount of characters in a name
+.set	MAXWAITSEC,        0 // The number in seconds to wait for connections
+.set	MAXWAITNSEC, 1000000 // The number in nanoseconds to wait (<1 second)
 
 .bss
 SERVER:	// The place to store the server's socket
 	.word	0
 ADDRESS:// The place to store the server's ip address
 	.word	0
+BUFFER:	// Where all of the read data is stored
+	.skip	BACKLOG
+
+.data
+TIMETOWAIT: // How long the server should wait for incoming data or clients
+	.word	MAXWAITSEC
+	.word	MAXWAITNSEC
+SERVERLISTENTEXT:
+	.asciz	"Listening for clients on port 0.0.0.0:7777"
 
 /*
 struct player
 {
-   byte status; (0=dead, 1=alive)
+   int status; (0=dead, 1=alive)
    int sockfd;
-   char* name;
+   char[16] name;
 }
 */
 
@@ -44,14 +57,51 @@ startNetServer:
 	ldr	r0, [r0]
 	bl	createServer
 
-	pop	{pc}		// Return
+	// Save the socket file descriptor for later
+	push	{r0}
 
-/* void serverTick(struct player* players[r0]) */
+	// Save the server's socket and ip address
+	mov32	r1, SERVER
+	str	r0, [r1]
+	mov32	r1, ADDRESS
+	movs	r0, #0
+	str	r0, [r1]
+
+	// Tell the user that we're now listening for clients
+	mov32	r1, SERVERLISTENTEXT
+	bl	prints
+
+	pop	{r0, pc}	// Return
+
+/* bool serverTick(struct player* players[r0]) */
 /* Advances the server by one tick */
-/* Data races: Reads and modifies the memory region players */
+/* Data races: Reads and modifies each of the players' memory regions */
 .thumb_func
 .global	serverTick
 .type	serverTick, %function
 serverTick:
+	push	{lr}		// Save return point for later
+
+	// Check if any clients want to join the server
+	bl	getIncomingClients
+
+	// Check if any of the players can do anything
+	bl	getIncomingData
+
+	pop	{pc}		// Return
+
+/* void	getIncomingClients() */
+/* Checks if clients want to join the server */
+/* Data Races: Reads and writes to each of the players' memory regions */
+.thumb_func
+getIncomingClients:
+	push	{lr}		// Save return point for later
+	pop	{pc}		// Return
+
+/* void	getIncomingData() */
+/* Checks all of the clients to see if they can do anything */
+/* Data Races: Reads each of the players' memory regions */
+.thumb_func
+getIncomingData:
 	push	{lr}		// Save return point for later
 	pop	{pc}		// Return
--- src/server/server.s
+++ src/server/server.s
@@ -1,7 +1,61 @@
-/* CONSTANTS */
-.set	PLRSPC, 2304	// How much space to allocate for player data
-
 .include "lib/macrolib/macrolib.inc"
+
+.bss
+.balign	2
+PLAYER_0:
+	.word	0	// Status
+	.word	0	// Socket File Descriptor
+	.skip	16	// Name
+PLAYER_1:
+	.skip	24	// See above for data layout
+PLAYER_2:
+	.skip	24	// See above for data layout
+PLAYER_3:
+	.skip	24	// See above for data layout
+PLAYER_4:
+	.skip	24	// See above for data layout
+PLAYER_5:
+	.skip	24	// See above for data layout
+PLAYER_6:
+	.skip	24	// See above for data layout
+PLAYER_7:
+	.skip	24	// See above for data layout
+PLAYER_8:
+	.skip	24	// See above for data layout
+PLAYER_9:
+	.skip	24	// See above for data layout
+PLAYER_A:
+	.skip	24	// See above for data layout
+PLAYER_B:
+	.skip	24	// See above for data layout
+PLAYER_C:
+	.skip	24	// See above for data layout
+PLAYER_D:
+	.skip	24	// See above for data layout
+PLAYER_E:
+	.skip	24	// See above for data layout
+PLAYER_F:
+	.skip	24	// See above for data layout
+
+.data
+.global	PLAYERS
+PLAYERS:
+	.word	PLAYER_0
+	.word	PLAYER_1
+	.word	PLAYER_2
+	.word	PLAYER_3
+	.word	PLAYER_4
+	.word	PLAYER_5
+	.word	PLAYER_6
+	.word	PLAYER_7
+	.word	PLAYER_8
+	.word	PLAYER_9
+	.word	PLAYER_A
+	.word	PLAYER_B
+	.word	PLAYER_C
+	.word	PLAYER_D
+	.word	PLAYER_E
+	.word	PLAYER_F
 
 .text
 .arm
@@ -20,51 +74,15 @@ main:
 	bl	startNetServer
 
 	// Allocate space for the players
-	subs	sp, #PLRSPC
-	mov	r4, sp
-	movs	r0, r4
-
-	// Zero out the player space
-	bl	zeroPlayers
+	mov32	r4, #PLAYERS
 .LmainLoop:
 	// Do one server tick
 	movs	r0, r4
 	bl	serverTick
-	cbnz	r0, .LoopEnd	// If the server tick returns zero then
+	cbnz	r0, .LoopEnd	// If the server tick returns non zero then
 	b	.LmainLoop	// Loop again
 .LoopEnd:
-	// Unallocate space from the players
-	adds	sp, #PLRSPC
 
 	movs	r0, #0		// exit(0)
 	b	sysExit
 
-/* void zeroPlayers(struct player* players[r0]) */
-.thumb_func
-.type	zeroPlayers, %function
-zeroPlayers:
-	push	{r4-r12, lr}
-
-	// Zero out registers
-	movs	r1, #0
-	movs	r2, #0
-	movs	r3, #0
-	movs	r4, #0
-	movs	r5, #0
-	movs	r6, #0
-	movs	r7, #0
-	movs	r8, #0
-	movs	r9, #0
-	movs	r10, #0
-	movs	r11, #0
-	movs	r12, #0
-
-	// Get endpoint
-	adds	lr, r0, #PLRSPC
-.LzeroLoop:
-	// Load instructions until we pass the maximum player space mark
-	stmia	r0!, {r1-r12}
-	cmp	r0, lr
-	blo	.LzeroLoop
-.LzeroEnd:
-	pop	{r4-r12, pc}
